8657fb17d06ba113edbc12bf4d0465851ff6bc61
dci-student <137389454+z-codez@users.noreply.github.com>
2024-11-26 07:19:57 +0100

    added addEntry function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
-const budget = document.querySelector("#budget");
-const calcButton = document.querySelector("#buttons button:first-of-type");
+const form = document.querySelector("#form");
+const budgetInput = document.querySelector("#budget");
+const entryDropdown = document.querySelector("#entry");
+const addEntryButton = document.querySelector("#controls button");
+// const calculateButton = document.querySelector("#buttons button:first-of-type");
+const clearButton = document.querySelector("#buttons button:last-of-type");
+const outputDisplay = document.querySelector("#display");
 
-calcButton.addEventListener("click", () => {
-  console.log(budget.value);
-})
+
+function addEntry() {
+  console.log(entryDropdown.value);
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const noOfEntries = targetInputContainer.querySelectorAll(`input[type="number"]`).length;
+
+  const currentEntry = entryDropdown.value + "-" + noOfEntries;
+
+  const HTMLString = `
+  <label for="${currentEntry}-name">Entry ${noOfEntries} Name</label>
+  <input id="${currentEntry}-name" type="text" placeholder="Name"/>
+  <label for="${currentEntry}-calories">Entry ${noOfEntries} Calories</label>
+  <input id="${currentEntry}-calories" type="number" min="0" placeholder="Number of calories"/>
+  `;
+
+  targetInputContainer.innerHTML += HTMLString;
+}
